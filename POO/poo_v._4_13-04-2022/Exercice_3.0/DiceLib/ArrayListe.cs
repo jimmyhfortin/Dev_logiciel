3f985430f0e654297e19effc27600eebadc12a6e
Jimmy H. Fortin <2210370@cshawi.ca>
2022-04-14 15:01:01 -0400

    14-04-2022

---
+++ POO/poo_v._4_13-04-2022/Exercice_3.0/DiceLib/ArrayListe.cs
@@ -0,0 +1,17 @@
+namespace DiceLib;
+
+public class ArrayListe
+{
+    public int Array { get; set; }
+    public int Liste { get; set; }
+
+    public ArrayListe(int array)
+    {
+        Array = array;
+    }
+
+    public ListeListe(int liste)
+    {
+        Liste = liste;
+    }
+}
