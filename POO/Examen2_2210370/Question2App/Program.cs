973e72d1e8e2d6255f8ff2c1980faabe4c2c1b22
Jimmy H. Fortin <2210370@cshawi.ca>
2022-05-26 14:57:38 -0400

    26-05-2022

--- POO/Examen2_2210370/Question2App/Program.cs
+++ POO/Examen2_2210370/Question2App/Program.cs
@@ -6,7 +6,11 @@ namespace Question2App
     {
         static void Main(string[] args)
         {
+            // Question #2 part-A is done and respect ALL the requirements (see QuestionLib2)___________________________
+            // Question #2 part-B_______________________________________________________________________________________
+            // Question #2 part-B.1
             var character = new List<Character>();
+             // Question #2 part-B.2
             Cleric character_1 = new Cleric(12,"The_Warrior666",20,12,10,16,8,20);
             Cleric character_2 = new Cleric("The_Warrior",8,16,15,3,19,3);
             Fighter character_3 = new Fighter(13,"The_Terminator",20,3,3,12,12,3);
@@ -19,6 +23,7 @@ namespace Question2App
             character.Add(character_4);
             character.Add(character_5);
             character.Add(character_6);
+             // Question #2 part-B.3
             foreach (var characterlist in character)
             {
                 Console.WriteLine(characterlist);
@@ -31,7 +36,7 @@ namespace Question2App
                 Console.WriteLine(character[i].Attack());
                 Console.WriteLine(character[i].Die());
             }
-            
+            // End
 
             
 
